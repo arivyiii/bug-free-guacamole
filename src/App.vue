3038a69d7ee641f7931b23fe264694650f2ca2d0
Allen Ivy <allen.ivy@pendo.io>
2025-06-11 14:16:49 -0400

    Add Account id

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import ConsoleSandbox from './components/ConsoleSandbox.vue'
 pendo.initialize({
         visitor: {
             id: 'VISITOR-UNIQUE-ID'
+        },
+        account: {
+            id: 'MyAccount'
         }
 });
 </script>
