7e51fd02e7e65c0d210caa65a36266e2b44916ca
Allen Ivy <allen.ivy@pendo.io>
2025-07-02 15:47:03 -0400

    Update router to use createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ConsoleSandbox from '../components/ConsoleSandbox.vue'
 import DataVisualizer from '../components/DataVisualizer.vue'
 import JzbDecoder from '../components/JzbDecoder.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
